e3b9e54d5fa49fec139151f4bd17609c4f83cd8a
robertldanaher-cyber <robertldanaher@gmail.com>
2026-03-16 07:33:00 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@
 //  Found in: Firebase Console > Project Settings > Your Apps
 // ============================================================
 
-const FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyAkCnNi-yP2-rz1VtzE-7yvHynZLZYy5KE",
+  authDomain: "wheel-of-misfortune-19ef4.firebaseapp.com",
+  databaseURL: "https://wheel-of-misfortune-19ef4-default-rtdb.firebaseio.com",
+  projectId: "wheel-of-misfortune-19ef4",
+  storageBucket: "wheel-of-misfortune-19ef4.firebasestorage.app",
+  messagingSenderId: "367444826595",
+  appId: "1:367444826595:web:cd8aac7acc88bb706628d6"
 };
 
 // ============================================================
